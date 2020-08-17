983dcb50b53ebaad1c42459d0fd0048855b4de7f
moi_queces30 <moi_queces30@192.168.0.4>
2020-08-17 10:57:42 -0600

    init commit

--- src/main/java/Principal.java
+++ src/main/java/Principal.java
@@ -15,39 +15,65 @@ public class Principal {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        
+        ConsulteCuenta cuenta = new ConsulteCuenta();
         IngresarMonto ingreso = new IngresarMonto();
         Retirar retiro = new Retirar();
+        chanPass cambiaCon = new chanPass();
+        TipoCambio cambio = new TipoCambio();
+        Recarga recarga = new Recarga();
         
         // TODO code application logic here
         
         
-        Usuarios usuario1 = new Usuarios("Moisés","Quesada", 20021 , 1234);
-        int op=Integer.parseInt(JOptionPane.showInputDialog(null, "Digite"
+        Usuarios usuario1 = new Usuarios("Moisés","Quesada", 20021 , "1234",100000);
+        
+       int i = 0;
+        do{
+            int id = Integer.parseInt(JOptionPane.showInputDialog("Digite su id"));
+            if(id != usuario1.getId()){
+                JOptionPane.showMessageDialog(null, "Su id es incorrecto");
+               
+            
+            }
+            else{
+              int cont=Integer.parseInt(JOptionPane.showInputDialog("Digite su contraseña"));
+              if(cont==Integer.parseInt(usuario1.getContrasena())){
+        int op=Integer.parseInt(JOptionPane.showInputDialog(null, "Digite "
              + " una de las opciones que "
-               + " muestra el menú : \n 1.Estado de Cuenta   2.Retiro\n "
-               + "3.Transferencia         4.Ingresar Monto\n 5.Tipo de Cambio"
-               + "      6.Recarga de celular\n 7.Cambiar contraseña   8.Salir"));
+               + " muestra el menú : \n 1.Estado de Cuenta   2.Ingresar Monto\n "
+               + "3.Transferencia         4.Retiro\n 5.Recarga de celular"
+               + "      6.Tipo de Cambio \n 7.Cambiar contraseña   8.Salir"));
         do{
        switch(op){ 
-           case 1:
+           case 1:cuenta.Cuenta(usuario1.getNombre(), usuario1.getApellido(), usuario1.getId(), usuario1.getSaldo());
                break;
-           case 2:
+           case 2: int monto1 = Integer.parseInt(JOptionPane.showInputDialog("Digite el monto a ingresar"));
+               int montoIngresado = ingreso.Ingresar(monto1);
+               usuario1.setSaldo(montoIngresado);
+               
+           
+           
                break;
            case 3:
                break;
-           case 4: 
+           case 4: int r = Integer.parseInt(JOptionPane.showInputDialog("Digite el monto que quiere retiar."));
+              int saldoRetirado =retiro.retirar(r, usuario1.getSaldo());
+              
            break;
-           case 5:
+           case 5:recarga.RecargaCel();
                break;
-           case 6:
+           case 6:cambio.Cambio();
                break;
            case 7:
+               String conNueva = cambiaCon.chanPass(usuario1.getContrasena());
+           usuario1.setContrasena(conNueva);
                break;
-           
+       
        }
        
        }while(op!=8);
+            }}
+        }while(i!=1);
     
 }
         
