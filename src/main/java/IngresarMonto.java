23bc80d3e42242e7b9f5364d917f7ae55682b1b5
moi_queces30 <moi_queces30@192.168.0.4>
2020-08-23 20:40:38 -0600

    init commit

--- src/main/java/IngresarMonto.java
+++ src/main/java/IngresarMonto.java
@@ -13,20 +13,25 @@ import javax.swing.JOptionPane;
  */
 public class IngresarMonto {
     
-    public int Ingresar(){
-        int monto1=0;
-      String monto = JOptionPane.showInputDialog("Digite la cantidad de dinero que desea ingresar.");
-        if((monto!=null) && !monto.equals("") && auxi.isNum(monto)){
-            monto1 = Integer.parseInt(monto);
+    public int Ingresar(int monto1){
+        
+       int monto = Integer.parseInt(JOptionPane.showInputDialog("Vuelva a digitar la cantidad de dinero que desea ingresar."));
+        if( (monto==monto1)){
+            
+            
             if(monto1>0){JOptionPane.showMessageDialog(null,"Ha ingresado un monto de : " + monto);
-                return monto1;
+                
             }
-            else{JOptionPane.showMessageDialog(null, "Debe ingresar un monto mayor !");
+            else if (monto1!=monto){JOptionPane.showMessageDialog(null, "El monto no coincide con el anterior !");
             
             }
+        }else if (monto1<10){
+            JOptionPane.showMessageDialog(null, "El monto debe ser mayor.");
+        
         }
        
   
-    return monto1;}
+    return monto;}
+
 
 }
